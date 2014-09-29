24481a40c855d71143fe242eaa0dcf1c850e380e
eight <eight04@gmail.com>
2014-09-29 14:32:20 +0800

    Implement smart gussing

--- README.md
+++ README.md
@@ -1,7 +1,20 @@
-Bunch of JavaScript
-===================
-Feel free to use them.
+Select text inside a link like Opera
+====================================
+Disable link draging and select text.
 
-Todos
------
-* [Linkify Plus Plus] Complete the top-level domain list.
+Info
+----
+With this script, you can force the firefox to select text inside links, instead of dragging them around. If you need to drag them, just hold the ctrl key when dragging.
+
+![](https://i.imgur.com/f7TgRur.png)
+![](https://i.imgur.com/NSqXG5n.png)
+
+
+Changelog
+---------
+* Version 3.1 (Sep 29, 2014)
+	- Now the script will only work if you drag horizontally.
+* Version 3.0.1 (Aug 17, 2014)
+	- Change the timing of uninit. Will uninit each time mouseup.
+* Version 3.0 (Aug 17, 2014)
+	- Rewrite with my coding style.
--- select-text-inside-a-link-like-opera.user.js
+++ select-text-inside-a-link-like-opera.user.js
@@ -4,87 +4,124 @@
 // @description Disable link draging and select text.
 // @include     http://*
 // @include     https://*
-// @version     3.0.1
+// @version     3.1
 // @grant		GM_addStyle
 // @run-at      document-start
 // ==/UserScript==
 
-/**
-
-With this script, you can force the firefox to select text inside links, 
-instead of dragging them around. If you need to drag them, just hold the ctrl 
-key when dragging.
-
-![](https://i.imgur.com/f7TgRur.png)
-![](https://i.imgur.com/NSqXG5n.png)
-
-*	Version 3.0.1 (Aug 17, 2014)
-	- Change the timing of uninit. Will uninit each time mouseup.
-*	Version 3.0 (Aug 17, 2014)
-	- Rewrite with my coding style.
-
- */
-
 var force = {
+	target: null,
+	select: getSelection(),
+	preventClick: false,
+	currentPos: {
+		x: null,
+		y: null
+	},
+	startPos: {
+		x: null,
+		y: null
+	},
 	handleEvent: function(e){
-		if(e.type == "click"){
-			if(this.preventClick){
+		if (e.type == "click") {
+		
+			if (this.preventClick) {
 				e.preventDefault();
 				e.stopPropagation();
 				this.preventClick = false;
 			}
-		}else if(e.type == "mousedown"){
-			if(e.button || e.ctrlKey || e.altKey || e.shiftKey){
+			
+		} else if (e.type == "mouseup") {
+		
+			if (!this.target) {
 				return;
-			}
-			if(e.target.nodeName == "IMG"){
-				return;
-			}
-			var a = e.target;
-			while(a.nodeName != "A" && a.nodeName != "HTML"){
-				a = a.parentNode;
-			}
-			if(!a.href){
-				return;
-			}
-			this.init(a);
-		}else if(e.type == "mouseup"){
-			if(!this.initialized){
-				return;
-			}
+			} 
 
-			if(getSelection().toString()){
+			if (this.select.toString()) {
 				this.preventClick = true;
 			}
 			
 			this.uninit();
+			
+		} else if (e.type == "mousemove") {
+			
+			this.currentPos.x = e.pageX;
+			this.currentPos.y = e.pageY;
+			
+			if (!this.target) {
+				return;
+			}
+
+			var caretPos = document.caretPositionFromPoint(this.currentPos.x, this.currentPos.y);
+			if (!this.multiSelect) {
+				this.select.extend(caretPos.offsetNode, caretPos.offset);
+			} else {
+				this.range.setEnd(caretPos.offsetNode, caretPos.offset);
+			}
+			
+		} else if (e.type == "dragstart") {
+		
+			if (e.button || e.altKey || e.shiftKey) {
+				return;
+			}
+			
+			if (e.target.nodeName == "IMG") {
+				return;
+			}
+			
+			var a = e.target;
+			while (a.nodeName != "A" && a.nodeName != "HTML") {
+				a = a.parentNode;
+			}
+			
+			if (!a.href) {
+				return;
+			}
+			
+			// console.log(this.currentPos.x - e.pageX, this.currentPos.y - e.pageY)
+			
+			if (Math.abs(e.pageX - this.currentPos.x) <= Math.abs(e.pageY - this.currentPos.y)) {
+				return;
+			}
+			
+			e.preventDefault();
+			this.init(e, a);
 		}
 	},
-	init: function(a){
-		this.initialized = true;
-		this.cached = a.getAttribute("draggable");
-		this.link = a;
+	init: function(e, a){
+	
+		this.target = a;
+		this.startPos.x = e.pageX;
+		this.startPos.y = e.pageY;
 		
-		a.classList.add("force-select");
-		a.draggable = false;
+		this.multiSelect = e.ctrlKey;
+		
+		var caretPos = document.caretPositionFromPoint(this.startPos.x, this.startPos.y);
+		if (!this.multiSelect) {
+			this.select.collapse(caretPos.offsetNode, caretPos.offset);
+		} else {
+			this.range = new Range();
+			this.range.setEnd(caretPos.offsetNode, caretPos.offset);
+			this.range.collapse();
+			this.select.addRange(this.range);
+		}
+		
+		this.target.classList.add("force-select");
+		
 	},
 	uninit: function(){
-		if(this.cached === null){
-			this.link.removeAttribute("draggable");
-		}else{
-			this.link.draggable = this.cached;
-		}
-		this.link.classList.remove("force-select");
+	
+		this.target.classList.remove("force-select");
+		this.target = null;
+		this.range = null;
+		this.multiSelect = false;
 		
-		this.initialized = false;
-		this.cached = null;
-		this.link = null;
 	}
 };
 
-document.addEventListener("mousedown", force, false);
+document.addEventListener("mousemove", force, false);
 document.addEventListener("mouseup", force, false);
 document.addEventListener("click", force, true);
+document.addEventListener("dragstart", force, true);
 document.addEventListener("DOMContentLoaded", function(){
 	GM_addStyle(".force-select{ -moz-user-select: text!important; }");
 }, false);
